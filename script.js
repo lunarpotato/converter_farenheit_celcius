3846544932d47a2b26e6654fb568429a18477499
lunarpotato <105643123+lunarpotato@users.noreply.github.com>
2023-10-08 19:34:16 +0200

    changed input fields and created output paragraphs created script with convert to fahrenheit and convert to celcius method

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const app = Vue.createApp({
+    data (){
+        return {
+            celciusInput:0,
+            fahrenheitInput:0,
+            celsiusResult: 0,
+            fahrenheitResult:0
+        };
+    },
+
+    methods: {
+        convertToFahrenheit(){
+            this.fahrenheitResult= this.celciusInput *1.8 +32;
+        },
+
+        convertToCelsius(){
+            this.celciusResult= (this.fahrenheitInput -32) /1.8;
+        },
+    },
 
 });
 
