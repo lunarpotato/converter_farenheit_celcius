c866083244a738eb44c671026c6a45678f760a4f
lunarpotato <105643123+lunarpotato@users.noreply.github.com>
2023-10-12 10:41:36 +0200

    changed layout, changed invalid input method and created output if no number is inserted, deleted red input border and font if input is not valid

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const app = Vue.createApp({
     data (){
         return {
-            celciusInput:0,
-            fahrenheitInput:0,
-            celsiusResult: 0,
-            fahrenheitResult:0
+            celsiusInput:'',
+            fahrenheitInput:'',
+            celsiusResult:0,
+            fahrenheitResult:0,
+            isInputValid: true,
         };
     },
 
     methods: {
         convertToFahrenheit(){
-            this.fahrenheitResult= this.celciusInput *1.8 +32;
+            const resultParagraph = document.getElementById('resultParagraph');
+            if (!isNaN(parseFloat(this.celsiusInput)) && isFinite(this.celsiusInput)) {
+                this.fahrenheitResult= this.celsiusInput *1.8 +32;
+                resultParagraph.textContent = this.fahrenheitResult + '°F';}
+            else {
+                resultParagraph.textContent = "Please insert a Number!"  
+            }
         },
 
         convertToCelsius(){
-            this.celciusResult= (this.fahrenheitInput -32) /1.8;
+            const resultParagraphB = document.getElementById('resultParagraphB');
+            if (!isNaN(parseFloat(this.fahrenheitInput)) && isFinite(this.fahrenheitInput)) {
+                this.celsiusResult= (this.fahrenheitInput -32) /1.8;
+                resultParagraphB.textContent = this.celsiusResult + '°C';}
+            else {
+                resultParagraphB.textContent = "Please insert a Number!" ;
+            }
+            
         },
     },
+    
 
 });
 
